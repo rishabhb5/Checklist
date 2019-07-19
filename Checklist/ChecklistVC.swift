c8ceeb4f42c20c225b16803692c85ab41a43e634
Rishabh Bendiganavale <rishabhb567@gmail.com>
2019-07-19 12:01:04 -0400

    Checklist complete with datasource and delegate methods.

---
+++ Checklist/ChecklistVC.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  Checklist
+//
+//  Created by Rishabh Bendiganavale on 7/19/19.
+//  Copyright © 2019 Rishabh Bendiganavale. All rights reserved.
+//
+
+import UIKit
+
+class ChecklistVC: UITableViewController {
+
+    let itemArray = ["A", "B", "C"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    //MARK - Tableview Datasource Methods
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ChecklistItemCell", for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+        return cell
+    }
+    
+    //MARK - TableView Delegate Methods
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // print(itemArray[indexPath.row])
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        }
+        else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+
+
+}
+
